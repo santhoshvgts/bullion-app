1c7506c528d22e1ef7541363c3051c8df7b3922b
gopikrishnavgts <104427526+gopikrishnavgts@users.noreply.github.com>
2023-09-20 16:50:42 +0530

    intro page 1

--- lib/ui/view/main/intro/intro_page.dart
+++ lib/ui/view/main/intro/intro_page.dart
@@ -3,6 +3,7 @@ import 'package:bullion/core/res/spacing.dart';
 import 'package:bullion/core/res/styles.dart';
 
 import 'package:bullion/ui/view/main/intro/intro_view_model.dart';
+import 'package:bullion/ui/view/main/intro/widgets/_page_indicator.dart';
 import 'package:bullion/ui/view/vgts_builder_widget.dart';
 import 'package:bullion/ui/widgets/button.dart';
 import 'package:flutter/material.dart';
@@ -13,11 +14,14 @@ class IntroPage extends VGTSBuilderWidget<IntroViewModel> {
   @override
   Widget viewBuilder(BuildContext context, locale, IntroViewModel viewModel, Widget? child) {
     return Scaffold(
+      //
+      //
       appBar: AppBar(
         elevation: 0,
         actions: [
           InkWell(
             onTap: viewModel.skipOnPressed,
+            overlayColor: MaterialStateProperty.all(Colors.transparent),
             child: Container(
               margin: const EdgeInsets.only(top: 12, right: 20),
               decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), border: Border.all(color: AppColor.border)),
@@ -32,12 +36,15 @@ class IntroPage extends VGTSBuilderWidget<IntroViewModel> {
           )
         ],
       ),
+      //
+      //
       body: SafeArea(
         child: PageView.builder(
           itemCount: viewModel.introSliderItems.length,
           controller: viewModel.pageController,
+          physics: const ClampingScrollPhysics(),
           onPageChanged: (value) {
-            viewModel.index = value;
+            viewModel.currentPageIndex = value;
             viewModel.notifyListeners();
           },
           itemBuilder: (context, index) => Padding(
@@ -64,9 +71,11 @@ class IntroPage extends VGTSBuilderWidget<IntroViewModel> {
           ),
         ),
       ),
+      //
+      //
       bottomNavigationBar: SafeArea(
         child: Wrap(children: [
-          PageIndicator(activeIndex: viewModel.index),
+          PageIndicator(activeIndex: viewModel.currentPageIndex),
           VerticalSpacing.custom(value: 33),
           Container(
             padding: EdgeInsets.only(left: 20, right: 20, bottom: viewModel.isLastindex ? 0 : 62),
@@ -75,28 +84,32 @@ class IntroPage extends VGTSBuilderWidget<IntroViewModel> {
           viewModel.isLastindex
               ? Padding(
                   padding: const EdgeInsets.only(top: 20, bottom: 21),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Don’t have an account ?',
-                        style: AppTextStyle.normal.copyWith(
-                          color: AppColor.text,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
+                  child: InkWell(
+                    overlayColor: MaterialStateProperty.all(Colors.transparent),
+                    onTap: viewModel.register,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Don’t have an account ?',
+                          style: AppTextStyle.normal.copyWith(
+                            color: AppColor.text,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
-                      ),
-                      HorizontalSpacing.custom(value: 8),
-                      Text(
-                        'Sign Up',
-                        style: AppTextStyle.normal.copyWith(
-                          color: AppColor.secondary,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      )
-                    ],
+                        HorizontalSpacing.custom(value: 8),
+                        Text(
+                          'Sign Up',
+                          style: AppTextStyle.normal.copyWith(
+                            color: AppColor.secondary,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 )
               : const SizedBox(),
@@ -108,48 +121,5 @@ class IntroPage extends VGTSBuilderWidget<IntroViewModel> {
   @override
   IntroViewModel viewModelBuilder(BuildContext context) {
     return IntroViewModel();
-  }
-}
-
-class Dot extends StatelessWidget {
-  final double size;
-  final Color color;
-
-  const Dot({super.key, this.size = 10.0, this.color = const Color(0xff626A7D)});
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-      width: size,
-      height: size,
-      decoration: BoxDecoration(
-        color: color,
-        shape: BoxShape.circle,
-      ),
-    );
-  }
-}
-
-class PageIndicator extends StatelessWidget {
-  final int activeIndex;
-
-  const PageIndicator({super.key, required this.activeIndex});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: List.generate(3, (index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 4.0),
-          child: Dot(
-            size: activeIndex == index ? 8.0 : 5.0,
-            color: const Color(0xFF626A7D).withOpacity(activeIndex == index ? 1.0 : 0.24),
-          ),
-        );
-      }),
-    );
   }
 }
--- lib/ui/view/main/intro/intro_view_model.dart
+++ lib/ui/view/main/intro/intro_view_model.dart
@@ -1,6 +1,5 @@
 import 'package:bullion/router.dart';
 import 'package:bullion/services/shared/eventbus_service.dart';
-import 'package:bullion/services/shared/navigator_service.dart';
 import 'package:bullion/ui/view/vgts_base_view_model.dart';
 import 'package:flutter/material.dart';
 
@@ -9,27 +8,34 @@ import '../../../../core/res/images.dart';
 import '../../../../locator.dart';
 
 class IntroViewModel extends VGTSBaseViewModel {
-  final NavigationService navigationService = locator<NavigationService>();
-  PageController? pageController;
-  int index = 0;
+  // Variables
+  int currentPageIndex = 0;
 
+  // Controllers
+  PageController? pageController;
+
+  // getters
+  String get _buttonName => introSliderItems.last == introSliderItems[currentPageIndex] ? "Get started" : "Next";
+  String get buttonName => _buttonName;
+
+  bool get _isLastindex => introSliderItems.last == introSliderItems[currentPageIndex] ? true : false;
+  bool get isLastindex => _isLastindex;
+
+  // Init
+  @override
+  Future onInit() {
+    pageController = PageController(initialPage: currentPageIndex);
+    return super.onInit();
+  }
+
+  // Dummy Data
   List<IntroSliderItem> introSliderItems = [
     IntroSliderItem(Images.priceAlerts, "Exclusive Deals", "Access to limited-time offers, discounts and promotions."),
     IntroSliderItem(Images.marketNews, "Order Tracking", "Easily track your shipments and stay updated on the delivery status."),
     IntroSliderItem(Images.vaultDeals, "Personalized Recommendations", "Receive tailored product suggestions based on your preferences."),
   ];
 
-  String get _buttonName => introSliderItems.last == introSliderItems[index] ? "Get started" : "Next";
-  String get buttonName => _buttonName;
-
-  bool get _isLastindex => introSliderItems.last == introSliderItems[index] ? true : false;
-  bool get isLastindex => _isLastindex;
-
-  @override
-  Future onInit() {
-    pageController = PageController(initialPage: index);
-    return super.onInit();
-  }
+  /* ----- Logics ------ */
 
   void skipOnPressed() {
     pageController?.animateToPage(introSliderItems.length - 1, duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
@@ -43,7 +49,7 @@ class IntroViewModel extends VGTSBaseViewModel {
       notifyListeners();
       return;
     }
-    locator<NavigationService>().popAllAndPushNamed(Routes.login);
+    navigationService.popAllAndPushNamed(Routes.login);
     return;
   }
 
@@ -52,15 +58,20 @@ class IntroViewModel extends VGTSBaseViewModel {
   }
 
   register() {
-    navigationService.pushNamed(
-      Routes.register,
-    );
+    navigationService.pushNamed(Routes.register);
   }
 
   continueWithoutLogin() {
     preferenceService.setFirstTimeAppOpen(false);
     navigationService.pushReplacementNamed(Routes.dashboard);
     locator<EventBusService>().eventBus.fire(RefreshDataEvent(RefreshType.homeRefresh));
+  }
+
+// Dispose
+  @override
+  void dispose() {
+    pageController?.dispose();
+    super.dispose();
   }
 }
 
