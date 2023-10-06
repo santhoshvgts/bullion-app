df0e79d748e751a4baf6f2ba3172028dc0961856
gopikrishnavgts <104427526+gopikrishnavgts@users.noreply.github.com>
2023-10-06 11:48:59 +0530

    Added logout button

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -3,10 +3,12 @@ import 'dart:async';
 import 'package:bullion/core/models/auth/auth_response.dart';
 import 'package:bullion/core/models/auth/user.dart';
 import 'package:bullion/locator.dart';
+import 'package:bullion/router.dart';
 import 'package:bullion/services/api_request/auth_request.dart';
 import 'package:bullion/services/shared/api_base_service.dart';
 import 'package:bullion/services/shared/api_model/error_response_exception.dart';
 import 'package:bullion/services/shared/dialog_service.dart';
+import 'package:bullion/services/shared/navigator_service.dart';
 import 'package:bullion/services/shared/preference_service.dart';
 import 'package:bullion/services/token_service.dart';
 
@@ -36,8 +38,7 @@ class AuthenticationService {
 
   void _setUser(AuthResponse authResult) {
     if ((authResult.token != null) && (authResult.user != null)) {
-      _tokenService.setTokens(
-          authResult.token!.authToken, authResult.token!.refreshToken);
+      _tokenService.setTokens(authResult.token!.authToken, authResult.token!.refreshToken);
       userController.add(authResult.user);
       _user = authResult.user;
       _analyticsService.setUserId(_user!.userId);
@@ -66,8 +67,7 @@ class AuthenticationService {
 
   Future<AuthResponse?> login(String email, String password) async {
     try {
-      var authResult = await _apiBaseService
-          .request<AuthResponse>(AuthRequest.login(email, password));
+      var authResult = await _apiBaseService.request<AuthResponse>(AuthRequest.login(email, password));
       _setUser(authResult);
       _analyticsService.loglogin();
 
@@ -96,6 +96,8 @@ class AuthenticationService {
 
     // TODO - Sentry Implementation
     // Sentry.configureScope((p0) => p0.setUser(null));
+
+    locator<NavigationService>().popAllAndPushNamed(Routes.introPage);
   }
 
   Future<AuthResponse?> register(
@@ -124,8 +126,7 @@ class AuthenticationService {
 
   Future<AuthResponse?> registerAsGuest(String email) async {
     try {
-      var authResult = await _apiBaseService
-          .request<AuthResponse>(AuthRequest.registerAsGuest(email));
+      var authResult = await _apiBaseService.request<AuthResponse>(AuthRequest.registerAsGuest(email));
       _setUser(authResult);
 
       return authResult;
@@ -142,8 +143,7 @@ class AuthenticationService {
     String oldPassword,
   ) async {
     try {
-      var authResult = await _apiBaseService.request<AuthResponse>(
-          AuthRequest.resetPassword(key, email, newPassword, oldPassword));
+      var authResult = await _apiBaseService.request<AuthResponse>(AuthRequest.resetPassword(key, email, newPassword, oldPassword));
       _setUser(authResult);
       _analyticsService.loglogin();
 
@@ -156,8 +156,7 @@ class AuthenticationService {
 
   Future<AuthResponse?> guestToAccount(String password, bool emailOptIn) async {
     try {
-      var authResult = await _apiBaseService.request<AuthResponse>(
-          AuthRequest.guestToAccount(password, emailOptIn));
+      var authResult = await _apiBaseService.request<AuthResponse>(AuthRequest.guestToAccount(password, emailOptIn));
       _setUser(authResult);
       _analyticsService.logSignUp();
 
