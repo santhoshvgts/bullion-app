ad4e063b3958f728748d76712bf3ff242aac3ff1
Anantha Eswar <anantha@vishgyana.com>
2023-10-16 19:35:23 +0530

    Settings/Account page - displays user info

--- lib/ui/view/dashboard/dashboard_view_model.dart
+++ lib/ui/view/dashboard/dashboard_view_model.dart
@@ -1,8 +1,9 @@
-import 'package:bullion/ui/order_details.dart';
 import 'package:bullion/ui/view/dashboard/content/dashboard_content_page.dart';
 import 'package:bullion/ui/view/vgts_base_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
+
+import '../settings/settings_user_page.dart';
 
 class DashboardViewModel extends VGTSBaseViewModel {
   final PersistentTabController bottomNavController =
@@ -17,7 +18,6 @@ class DashboardViewModel extends VGTSBaseViewModel {
         key: PageStorageKey('Charts'), path: "/spot-prices"),
     const DashboardContentPage(
         key: PageStorageKey('Spot Price'), path: "/pages/deals"),
-    //const SettingsUserPage()
-     OrderDetails()
+    const SettingsUserPage()
   ];
 }
--- lib/ui/view/settings/settings_user_view_model.dart
+++ lib/ui/view/settings/settings_user_view_model.dart
@@ -1,3 +1,17 @@
+import '../../../locator.dart';
+import '../../../services/authentication_service.dart';
 import '../vgts_base_view_model.dart';
 
-class SettingsUserViewModel extends VGTSBaseViewModel {}
+class SettingsUserViewModel extends VGTSBaseViewModel {
+  bool _isAuthenticated = false;
+
+  SettingsUserViewModel() {
+    init();
+  }
+
+  void init() {
+    _isAuthenticated = locator<AuthenticationService>().isAuthenticated;
+  }
+
+  bool get isAuthenticated => _isAuthenticated;
+}
