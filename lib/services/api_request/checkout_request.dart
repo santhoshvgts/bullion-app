8d17a549b24a5871fd23bb90788b503175a57c59
gopikrishnavgts <104427526+gopikrishnavgts@users.noreply.github.com>
2023-12-15 11:37:03 +0530

    checkout page

---
+++ lib/services/api_request/checkout_request.dart
@@ -0,0 +1,73 @@
+import 'package:bullion/core/models/module/checkout/selected_payment_method.dart';
+import 'package:bullion/core/models/module/checkout/shipping_address.dart';
+import 'package:bullion/services/shared/api_model/request_settings.dart';
+import 'package:bullion/services/shared/request_method.dart';
+
+class CheckOutRequest {
+  //
+
+  static RequestSettings getCheckout() {
+    return RequestSettings("/checkout/get", RequestMethod.GET, params: null, authenticated: true);
+  }
+
+  //
+
+  static RequestSettings validatePrice() {
+    return RequestSettings("/checkout/validate-cart", RequestMethod.GET, params: null, authenticated: true);
+  }
+
+  //
+
+  static RequestSettings getPaymentMethods() {
+    return RequestSettings("/checkout/get-payment-methods", RequestMethod.GET, params: null, authenticated: true);
+  }
+
+  //
+
+  static RequestSettings saveDeliveryAddress({int? addressId, bool? isCitadel, String? citadelAccount}) {
+    String queryString = "?";
+    queryString += "addressId=$addressId";
+    queryString += "&isCitadel=$isCitadel";
+    queryString += "&citadelAccount=$citadelAccount";
+
+    return RequestSettings("/checkout/save-address$queryString", RequestMethod.POST, params: null, authenticated: true);
+  }
+
+  //
+
+  static RequestSettings saveShippingOption({int? shippingId, double? shipCharge}) {
+    String queryString = "?";
+
+    queryString += "shippingId=$shippingId";
+    queryString += "&shipCharge=$shipCharge";
+
+    return RequestSettings("/checkout/save-shipping-option$queryString", RequestMethod.POST, params: null, authenticated: true);
+  }
+
+  //
+
+  static RequestSettings placeOrder(ShippingAddress? shippingAddress, SelectedPaymentMethod? paymentMethod, String? paymentMethodNonce, String? paymentMethodDeviceData) {
+    Map<String, dynamic> params = {};
+    params['selected_address_id'] = shippingAddress == null
+        ? null
+        : shippingAddress.isCitadel!
+            ? null
+            : shippingAddress.address!.id;
+    params['is_citadel'] = shippingAddress == null ? false : shippingAddress.isCitadel;
+    params['selected_payment_id'] = paymentMethod?.paymentMethodId;
+    params['payment_method_nounce'] = paymentMethodNonce;
+    params['payment_method_device_data'] = paymentMethodDeviceData;
+
+    return RequestSettings("/checkout/place-order", RequestMethod.POST, params: params, authenticated: true);
+  }
+
+  //
+
+  static RequestSettings removeDiscountByKey(String? keyCode) {
+    String queryString = "?";
+
+    queryString += "keycode=$keyCode";
+
+    return RequestSettings("/checkout/remove-discounts-by-key$queryString", RequestMethod.POST, params: null, authenticated: true);
+  }
+}
