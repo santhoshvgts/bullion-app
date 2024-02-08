9ecf1c3147e0982bc42bbac011db912aee903205
santhoshvgts <48829088+santhoshvgts@users.noreply.github.com>
2024-02-08 17:20:02 +0530

    * DeepLink Configuration in iOS

--- lib/ui/view/checkout/credit_card/credit_card_bottomsheet_view_model.dart
+++ lib/ui/view/checkout/credit_card/credit_card_bottomsheet_view_model.dart
@@ -1,13 +1,13 @@
 import 'package:bullion/helper/utils.dart';
 import 'package:bullion/ui/view/vgts_base_view_model.dart';
 import 'package:credit_card_scanner/credit_card_scanner.dart';
+import 'package:credit_card_scanner/models/card_details.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:bullion/core/models/alert/alert_response.dart';
 import 'package:bullion/locator.dart';
 import 'package:bullion/services/shared/dialog_service.dart';
 import 'package:vgts_plugin/form/utils/form_field_controller.dart';
-import '../../../../../core/enums/viewstate.dart';
 import '../../../../../helper/card_utils.dart';
 
 class CreditCardViewModel extends VGTSBaseViewModel {
